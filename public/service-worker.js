b32b50c5fa103baf7760f5a5b93d498ac095fdd6
Dinimar Fish <tayla@tallisman.com.au>
2021-10-16 16:05:59 +1000

    added cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,15 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v2";
-const FILES_TO_CACHE = [];
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.webmanifest",
+  "/index.js",
+  "/indexedDb.js",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
 
 // install
 self.addEventListener("install", function (evt) {
