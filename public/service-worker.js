a569788a1dcbde65e7b5b6faca32c73ee227453e
Dinimar Fish <tayla@tallisman.com.au>
2021-10-16 15:59:07 +1000

    initial commit

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v2";
+const FILES_TO_CACHE = [];
+
+// install
+self.addEventListener("install", function (evt) {
+    // pre cache image data
+    evt.waitUntil(
+      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transactions"))
+    );
+
+    // pre cache all static assets
+  evt.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+
+  self.skipWaiting();
+
+  });
