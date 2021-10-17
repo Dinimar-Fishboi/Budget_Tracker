b03bcf7c7e46b358516c12b65f5cec866ffca3f7
Dinimar Fish <tayla@tallisman.com.au>
2021-10-17 19:06:55 +1000

    indexedDB structure added to store data offline

--- public/indexedDb.js
+++ public/indexedDb.js
@@ -1,13 +1,73 @@
 //IndexDB for when server is offline
+let db;
+let offlineBudget;
 
-export const saveRecord = (record) => {
-    console.log('Save record invoked');
-    // Create a transaction on the BudgetStore db with readwrite access
-    const transaction = db.transaction(['BudgetStore'], 'readwrite');
+// Creating a space to store the info when offline
+const request = indexedDB.open('BudgetDB', offlineBudget || 21);
+
+request.onupgradeneeded = function (event) {
+  console.log('Upgrade needed in IndexDB');
+
+  db = event.target.result;
+
+  if (db.objectStoreNames.length === 0) {
+    db.createObjectStore('BudgetStore', { autoIncrement: true });
+  }
+};
+
+function checkDatabase() {
+    console.log('Checking offline db');
   
-    // Access your BudgetStore object store
+    let transaction = db.transaction(['BudgetStore'], 'readwrite');
+  
     const store = transaction.objectStore('BudgetStore');
   
-    // Add record to your store with add method.
+    const getAll = store.getAll();
+  
+    getAll.onsuccess = function () {
+
+        if (getAll.result.length > 0) {
+        fetch('/api/transaction/bulk', {
+          method: 'POST',
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((response) => response.json())
+          .then((res) => {
+            if (res.length !== 0) {
+              transaction = db.transaction(['BudgetStore'], 'readwrite');
+  
+              const currentStore = transaction.objectStore('BudgetStore');
+  
+              currentStore.clear();
+              console.log('Store now empty: checkDatabase has run');
+            }
+          });
+      }
+    };
+  }
+  
+  request.onsuccess = function (event) {
+    console.log('success');
+    db = event.target.result;
+  
+    if (navigator.onLine) {
+      console.log('Back online');
+      checkDatabase();
+    }
+  };
+  
+
+// Need to define Transaction
+
+ const saveRecord = (record) => {
+    console.log('Save record invoked');
+    const transaction = db.transaction(['BudgetStore'], 'readwrite');
+  
+    const store = transaction.objectStore('BudgetStore');
+  
     store.add(record);
   };
