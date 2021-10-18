26db5b71244e9bbb62eb8d922dc1e6f70d4f2eb7
Dinimar Fish <tayla@tallisman.com.au>
2021-10-18 21:27:04 +1000

    added activate and fetch listeners

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v2";
+const CACHE_NAME = "static-cache-v3";
+const DATA_CACHE_NAME = "data-cache-v3";
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
@@ -8,21 +8,50 @@ const FILES_TO_CACHE = [
   "/indexedDb.js",
   "/styles.css",
   "/icons/icon-192x192.png",
-  "/icons/icon-512x512.png"
+  "/icons/icon-512x512.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
 
 // install
 self.addEventListener("install", function (evt) {
-    // pre cache image data
-    evt.waitUntil(
-      caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/transactions"))
-    );
 
-    // pre cache all static assets
   evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-  );
-
-  self.skipWaiting();
+    caches.open(CACHE_NAME)
+    .then((cache) => cache.addAll(FILES_TO_CACHE))
+    )
+    self.skipWaiting();
 
   });
+
+  // activate
+self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys()
+      .then((keyList) => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    )
+    .then(self.clients.claim());
+  });
+
+  // fetch
+self.addEventListener("fetch", function(evt) {
+      evt.respondWith(
+          fetch(evt.request)
+            .catch(() => {
+                return caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.match(evt.request);
+            });
+        })
+      );
+  
+    });
